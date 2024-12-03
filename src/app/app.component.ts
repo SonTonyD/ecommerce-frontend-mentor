e3bf67b8d2bc3f156aef457fdf521728331645fa
st.dinh <sontony.dinh@gmail.com>
2024-12-03 14:16:25 +0100

    lightbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -12,7 +13,43 @@ export class AppComponent {
 
   count = 0;
 
+  imagesThumbnail = [
+    'image-product-1-thumbnail.jpg',
+    'image-product-2-thumbnail.jpg',
+    'image-product-3-thumbnail.jpg',
+    'image-product-4-thumbnail.jpg',
+  ];
+
+  imagesProduct = [
+    'image-product-1.jpg',
+    'image-product-2.jpg',
+    'image-product-3.jpg',
+    'image-product-4.jpg',
+  ];
+
+  selectedImageIndex: number | null = null;
+
+  selectImage(index: number): void {
+    this.selectedImageIndex = index;
+    this.lightboxImage = this.imagesProduct[index];
+
+    console.log(this.selectedImageIndex !== null);
+
+    console.log(this.imagesProduct[this.selectedImageIndex + 1] !== undefined);
+  }
+
   setCounter(number: number) {
     this.count = number;
+  }
+
+  lightboxImage: string | null = null;
+
+  openLightbox(image: string): void {
+    this.lightboxImage = image;
+  }
+
+  closeLightbox(): void {
+    this.lightboxImage = null;
+    this.selectedImageIndex = null;
   }
 }
