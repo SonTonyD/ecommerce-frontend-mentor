52084d00ef9af17757092d5128ee5ba60ab40569
st.dinh <sontony.dinh@gmail.com>
2024-12-03 14:41:15 +0100

    polish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgIf, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
