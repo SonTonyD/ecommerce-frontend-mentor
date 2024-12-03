b4789a8bd1d26cce633eeef3a8c70b1c95d4a398
st.dinh <sontony.dinh@gmail.com>
2024-12-03 10:51:51 +0100

    desktop and mobile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'frontend-mentor-e-commerce';
+
+  count = 0;
+
+  setCounter(number: number) {
+    this.count = number;
+  }
 }
